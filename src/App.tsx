22544b791834f997544d61c4fc7d0afe2e71a48c
Vineeth <vineeth.v.gooli@gmail.com>
2023-07-12 16:41:45 +0530

    removed worlds

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import BoxCanvas from "./Components/BoxCanvas";
-import Canvas from "./Components/Canvas";
 
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Canvas />} />
-          <Route path="/box" element={<BoxCanvas />} />
-        </Routes>
-      </Router>
+      <BoxCanvas />
     </>
   );
 }
